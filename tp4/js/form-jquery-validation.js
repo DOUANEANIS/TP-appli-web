7aeac7f65194df2b9b267665e2018fbae97b15a5
DOUANEANIS <anis.douane@outlook.fr>
2020-11-24 18:48:50 +0100

    Update form-jquery-validation.js

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -1,48 +1,60 @@
 $(document).ready(function(e) {
  
-    $(document).keyup(function(){
-  
-      var nombreCaractere = $("#name").val().length;
-      var msg = nombreCaractere + ' car';
-  
-      $('#nameCounter1').text(msg);
-  
-  
-      var nombreCaractere2 = $("#firstname").val().length;
-      var msg2 = nombreCaractere2 + ' car';
-  
-      $('#nameCounter2').text(msg2);
-      
-    });
-  
-   
-      
+  $(document).keyup(function(){
+
+    var nombreCaractere = $("#name").val().length;
+    var msg = nombreCaractere + ' car';
+
+    $('#compt1').text(msg);
+
+
+    var nombreCaractere2 = $("#firstname").val().length;
+    var msg2 = nombreCaractere2 + ' car';
+
+    $('#compt2').text(msg2);
     
-    document.querySelector("form").addEventListener("submit", function (event) {
-      event.preventDefault();
-      contactStore.add(
-        document.querySelector("#name").value,
-        document.querySelector("#firstname").value,
-        document.querySelector("#birth").value,
-        document.querySelector("#adresse").value,
-        document.querySelector("#mail").value
-      );
-  
-      const contactList = contactStore.getList();
-  
-      document.querySelector("table tbody").innerHTML = "";
-  
-      for (var contact of contactList) {
-        document.querySelector("table tbody").innerHTML += `
-          <tr>
-              <td> ${contact.name}</td>
-              <td> ${contact.firstname}</td>
-              <td> ${contact.date}</td>
-              <td> <a href="https://maps.google.com/?q=${contact.adress}">${contact.adress}</a></td>
-              <td> <a href="mailto:${contact.mail}">${contact.mail}</a></td>
-          <tr>`;
-      }
-    });
   });
 
-  
+  $("#valide").on("click",function store(event) { 
+    event.preventDefault();
+    
+        var inputNom= document.getElementById("name");
+        var inputPrenom= document.getElementById("firstname");
+        var inputDn= document.getElementById("birth");
+        var inputAdresse= document.getElementById("adresse");
+        var inputEmail= document.getElementById("mail");
+        
+        if($("#name").val() !== "" && $("#firstname").val() !== "" && $("#birth").val() !== "" && $("#adresse").val() !== "" && $("#mail").val() !== "" ){
+        
+        localStorage.setItem("name", inputNom.value);
+        localStorage.setItem("firstname", inputPrenom.value);
+        localStorage.setItem("birth", inputDn.value);
+        localStorage.setItem("adresse", inputAdresse.value);
+        localStorage.setItem("mail", inputEmail.value);
+
+       $('#success').addClass("alert alert-success").text("Bravo! le formulaire est sauvegardé.");
+
+      $("#table").show();
+      $("#liste_des_contacts").show()
+       
+       document.querySelector("table tbody").innerHTML = document.querySelector("table tbody")
+       .innerHTML +'<tr><td>'+localStorage.getItem("name")+'</td><td>'+localStorage.getItem("firstname") +
+       '</td><td>'+localStorage.getItem("birth")+ '</td><td><a href="https://maps.google.com/maps?q=' +localStorage.getItem("adresse")+'">'+localStorage.getItem("adresse") +'</a></td><td><a href=mailto:>'+localStorage.getItem("mail")+'</a></td>';
+       }
+      
+       document.querySelector("table tbody").innerHTML = document.querySelector("table tbody")
+       .innerHTML +'<tr><td>'+contactStore.getList.name.val()+
+       '</td><td>'+localStorage.getItem("firstname") +
+       '</td><td>'+localStorage.getItem("birth")+
+       '</td><td><a href="https://maps.google.com/maps?q=' +localStorage.getItem("adresse")+'">'+localStorage.getItem("adresse") +'</a></td><td><a href=mailto:>'+localStorage.getItem("mail")+'</a></td>';
+       
+     
+       
+      
+  });
+
+ 
+
+}); 
+   
+ 
